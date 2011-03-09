920c1bbfe9c63a453a3ef6e220e0bb1ca9fa04c0
paj2s <devnull@localhost>
2011-03-09 13:06:09 -0500

    single_gene_ko added

---
+++ single_gene_ko.m
@@ -0,0 +1,23 @@
+function [grRatio,grRateKO,grRateWT] = single_gene_ko(tiger,genes)
+
+if nargin < 2
+    genes = tiger.genes;
+end
+
+N = length(genes);
+
+grRatio  = zeros(N,1);
+grRateKO = zeros(N,1);
+
+sol = fba(tiger);
+grRateWT = sol.val;
+
+[~,loc] = ismember(genes,tiger.varnames);
+for i = 1 : N
+    m = tiger;
+    m.ub(loc(i)) = 0;
+    sol = fba(m);
+    grRateKO(i) = sol.val;
+end
+
+grRatio = grRateKO / grRateWT;
