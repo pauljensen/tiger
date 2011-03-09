2d31be7972960bbc2f443771fa9dc2e06308b14f
paj2s <devnull@localhost>
2011-03-09 16:22:59 -0500

    fixed bounds in bind_var

--- bind_var.m
+++ bind_var.m
@@ -1,4 +1,20 @@
-function [tiger] = bind_var(tiger,vars,inds)
+function [tiger] = bind_var(tiger,vars,inds,varargin)
+
+p = inputParser;
+p.addParamValue('tight',false);
+p.addParamValue('lb',[]);
+p.addParamValue('ub',[]);
+p.parse(varargin{:});
+
+tight = p.Results.tight;
+default_lb = p.Results.lb;
+default_ub = p.Results.ub;
+if isempty(default_lb)
+    default_lb = min(tiger.lb);
+end
+if isempty(default_ub)
+    default_ub = max(tiger.ub);
+end
 
 [~,vars,~] = convert_ids(tiger.varnames,vars);
 [~,inds,~] = convert_ids(tiger.varnames,inds);
@@ -12,8 +28,15 @@ d = zeros(2*N,1);
 ctypes = [repmat('<',N,1); repmat('>',N,1)];
 
 for i = 1 : N
-    A(  i,[vars(i) inds(i)]) = [1 -tiger.ub(vars(i))];
-    A(i+N,[vars(i) inds(i)]) = [1 -tiger.lb(vars(i))];
+    if tight
+        ub = tiger.ub(vars(i));
+        lb = tiger.lb(vars(i));
+    else
+        lb = default_lb;
+        ub = default_ub;
+    end
+    A(  i,[vars(i) inds(i)]) = [1 -ub];
+    A(i+N,[vars(i) inds(i)]) = [1 -lb];
     rownames{i} = sprintf('BIND%i',i);
     rownames{i+N} = rownames{i};
 end
