24560eac3083de90308ccc385e562b9210082a02
paj2s <devnull@localhost>
2011-08-08 11:38:00 -0400

    added about_tiger.m

---
+++ about_tiger.m
@@ -0,0 +1,15 @@
+function about_tiger()
+
+VERSION = '0.9.1';
+
+fprintf('\n\n');
+fprintf('TIGER:  Toolbox for Integrating Genome-scale metabolism,\n');
+fprintf('        Expression, and Regulation\n\n');
+fprintf('Version:  %s\n\n',VERSION);
+fprintf('TIGER is maintained by the Computational Systems Biology\n');
+fprintf('Laboratory, Department of Biomedical Engineering, University\n');
+fprintf('of Virginia.\n\n');
+fprintf('Questions, comments, and suggestions can be sent to \n');
+fprintf('tigerproject@email.virginia.edu.\n\n');
+fprintf('Source code and documentation is available online at\n');
+fprintf('http://csbl.bitbucket.org/tiger\n\n');
