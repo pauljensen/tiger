3794da3767a1a4602736d24bebd8c7fcfd7ac4ff
paj2s <devnull@localhost>
2011-08-29 21:59:56 -0400

    fixed and extended flatten.m

--- util/flatten.m
+++ util/flatten.m
@@ -1,8 +1,26 @@
-function [list] = flatten(lists)
+function [list] = flatten(lists,depth)
 % FLATTEN  Flatten a cell of cells into a single cell.
+%
+%   [LIST] = FLATTEN(LISTS) flattens a cell of cells into a single cell.
+%   If no element of LISTS is a cell, LIST = LISTS.
+%
+%   [LIST] = FLATTEN(LISTS,DEPTH) flattens to a depth of DEPTH.  The
+%   default depth is 1.
 %
 %   >> A = FLATTEN({{1,2},3,{4}})
 %   A = 
 %       [1]  [2]  [3]  [4]
 
-list = [lists{:}];
+if nargin < 2
+    depth = 1;
+end
+
+if depth > 1
+    lists = flatten(lists,depth-1);
+end
+
+if ~any(cellfun(@iscell,lists))
+    list = lists;
+else
+    list = [lists{:}];
+end
