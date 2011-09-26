e46ac3145fcc704bbb21957c7c7d9728f0e2ca4b
paj2s <devnull@localhost>
2011-09-25 21:13:45 -0400

    update about_tiger

--- about_tiger.m
+++ about_tiger.m
@@ -1,6 +1,6 @@
 function about_tiger()
 
-VERSION = '0.9.1 (08/05/2011)';
+VERSION = '1.0.0 (09/25/2011)';
 
 fprintf('\n\n');
 fprintf('TIGER:  Toolbox for Integrating Genome-scale metabolism,\n');
@@ -21,4 +21,4 @@ fprintf('http://csbl.bitbucket.org/tiger\n\n');
 fprintf('Citation information:\n');
 fprintf('   Jensen P.A., K.A. Lutz, J.A. Papin.  TIGER:  Toolbox for integrating\n');
 fprintf('   genome-scale metabolic models, expression data, and trascriptional\n');
-fprintf('   regulatory networks.  BMC Sys. Biol. 2011.\n\n');
+fprintf('   regulatory networks.  BMC Systems Biology (2011) 5:147\n\n');
